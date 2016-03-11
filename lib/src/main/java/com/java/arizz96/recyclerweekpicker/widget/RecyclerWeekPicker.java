9bcc6b100fa357a5680dbf5564ebd03d3e8f9dd6
arizz <arizz96@gmail.com>
2016-03-11 21:26:06 +0100

    added RecyclerViewPicker class

---
+++ lib/src/main/java/com/java/arizz96/recyclerweekpicker/widget/RecyclerWeekPicker.java
@@ -0,0 +1,23 @@
+package com.java.arizz96.recyclerweekpicker.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.lsjwzh.widget.recyclerviewpager.RecyclerViewPager;
+
+/**
+ * Created by arizz on 11/03/2016.
+ */
+public class RecyclerWeekPicker extends RecyclerViewPager {
+    public RecyclerWeekPicker(Context context) {
+        super(context);
+    }
+
+    public RecyclerWeekPicker(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RecyclerWeekPicker(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
