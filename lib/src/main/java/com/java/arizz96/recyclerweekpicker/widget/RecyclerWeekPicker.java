0b0e9e899e9060524a4402eb8fdc195e18182c46
arizz <arizz96@gmail.com>
2016-03-13 13:52:59 +0100

    added recycler week picker class

--- lib/src/main/java/com/java/arizz96/recyclerweekpicker/widget/RecyclerWeekPicker.java
+++ lib/src/main/java/com/java/arizz96/recyclerweekpicker/widget/RecyclerWeekPicker.java
@@ -1,23 +1,88 @@
 package com.java.arizz96.recyclerweekpicker.widget;
 
 import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.java.arizz96.recyclerweekpicker.R;
 import com.lsjwzh.widget.recyclerviewpager.RecyclerViewPager;
+
+import java.util.ArrayList;
+import java.util.Calendar;
 
 /**
  * Created by arizz on 11/03/2016.
  */
-public class RecyclerWeekPicker extends RecyclerViewPager {
+public class RecyclerWeekPicker extends RelativeLayout {
+
+    private TextView mMonthTxv;
+    private RecyclerViewPager mWeekPickerRvp;
+    private onDaySelected mDaySelected;
+
+    public static interface onDaySelected {
+        void onSelected(Calendar day);
+    }
+
     public RecyclerWeekPicker(Context context) {
         super(context);
+        setupView();
     }
 
     public RecyclerWeekPicker(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setupView();
     }
 
     public RecyclerWeekPicker(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        setupView();
+    }
+
+    private void inflateView() {
+        inflate(getContext(), R.layout.week_picker_item, this);
+        mMonthTxv = (TextView) findViewById(R.id.week_picker_item_monthTxv);
+        mWeekPickerRvp = (RecyclerViewPager) findViewById(R.id.week_picker_item_weekRvp);
+    }
+
+    private void setupView() {
+        inflateView();
+        Calendar currentDay = Calendar.getInstance();
+        ArrayList<WeekItem> weeks = Utils.getWeeksForMonth(currentDay);
+        RecyclerWeekPickerAdapter.clickInterface listener = new RecyclerWeekPickerAdapter.clickInterface() {
+            @Override
+            public void onClick(Calendar calendar) {
+                getOnDaySelectedListener().onSelected(calendar);
+            }
+        };
+
+        RecyclerWeekPickerAdapter adapter = new RecyclerWeekPickerAdapter(getContext(), weeks, listener);
+        mWeekPickerRvp.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+        mWeekPickerRvp.setAdapter(adapter);
+        mWeekPickerRvp.addOnPageChangedListener(new RecyclerViewPager.OnPageChangedListener() {
+            @Override
+            public void OnPageChanged(int i, int i1) {
+                mMonthTxv.setText(((RecyclerWeekPickerAdapter) mWeekPickerRvp.getAdapter()).getMonthNameForWeek(i1));
+            }
+        });
+    }
+
+    public void setOnDaySelectedListener(onDaySelected listener) {
+        mDaySelected = listener;
+    }
+
+    public onDaySelected getOnDaySelectedListener() {
+        if(mDaySelected == null)
+            return  new onDaySelected() {
+                @Override
+                public void onSelected(Calendar day) {
+
+                }
+            };
+        else
+            return mDaySelected;
     }
 }
